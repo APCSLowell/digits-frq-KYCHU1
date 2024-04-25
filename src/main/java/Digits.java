264f61448fd491a96b36e979850bf40d355665a3
KYCHU1 <142847721+KYCHU1@users.noreply.github.com>
2024-04-24 20:54:00 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,29 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+		digitList = new ArrayList<Integer>();
+		if(num == 0)
+			digitList.add(0);
+		int n = num;
+		while(n > 0){
+			digitList.add(0, n%10);
+			n=n/10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		int count = 0;
+		for(int i = 0; i < digitList.size()-1; i++){
+			if(digitList.get(i) < digitList.get(i+1){
+				count++;
+			}   
+		}
+		if(count == digitList.size()-1){
+			return true;
+		} else{
+			return false;
+		}
 	}
 	
 	public String toString()
